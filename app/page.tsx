3c1f0b5a60b2dcdabfa8d14886bf036c082fa480
catdevdog <catdevdog@gmail.com>
2025-06-11 21:56:38 +0900

    test: notion data test

--- app/page.tsx
+++ app/page.tsx
@@ -151,11 +151,6 @@ export default function Home() {
     handlers.start();
     handlers.profile();
     updateVh();
-    fetch("/api/notion")
-      .then((res) => res.json())
-      .then((data) => {
-        console.log("Fetched data:", data);
-      });
   }, []);
 
   return (
--- components/content/Project.tsx
+++ components/content/Project.tsx
@@ -1,10 +1,35 @@
 import * as S from "./Content.styles";
+import { useState, useEffect } from "react";
 
 export const Project = () => {
+  const [projectData, setProjectData] = useState<any[]>([]);
+
+  useEffect(() => {
+    fetch("/api/notion")
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.results) {
+          setProjectData(data.results);
+          console.log("Project data fetched successfully:", data.results);
+        } else {
+          console.error("No results found in the response");
+        }
+      });
+  }, []);
   return (
     <S.ContentContainer>
       <h1>Project</h1>
       <p>참여한 프로젝트 내용이 여기에 표시됩니다.</p>
+      {projectData.map((project, idx) => {
+        return (
+          <div key={idx}>
+            <h2>{project.id}</h2>
+            {Object.keys(project.properties).map((key) => {
+              return key;
+            })}
+          </div>
+        );
+      })}
     </S.ContentContainer>
   );
 };
